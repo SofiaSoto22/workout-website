779bca032bf8dcf696aa0696546e0d9f8a0bdb94
sofia <sofiasoto3b@gmail.com>
2024-01-13 11:59:00 +0100

    dropdown menu, capitalization fix, column width adjustments

--- workout-index.js
+++ workout-index.js
@@ -9,45 +9,45 @@ let exercise = []
 const urlParams = new URLSearchParams(window.location.search);
 const queryParam = urlParams.get('type');
 const apiURL = "https://api.api-ninjas.com/v1/exercises?type="+ queryParam;
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 document.addEventListener('DOMContentLoaded', async function(){
   try{
     const response =  await fetch(apiURL, options)
     const data = await response.json();
     console.log(data);
     for (let i=0; i<data.length; i++){
-      let exerciseName = data[i].name.replaceAll("-", " ");
-      let exerciseEquipment = data[i].equipment.replaceAll("_", "");
-      let exerciseMuscle = data[i].muscle;
-      let exerciseInstructions = data[i].instructions;
+      let exerciseName = capitalizeFirstLetter(data[i].name.replaceAll("-", " "));
+      let exerciseEquipment = capitalizeFirstLetter(data[i].equipment.replaceAll("_", " "));
+      let exerciseMuscle = capitalizeFirstLetter(data[i].muscle);
+      let exerciseInstructions = capitalizeFirstLetter(data[i].instructions);
       exercise.push([exerciseName, exerciseEquipment, exerciseMuscle, exerciseInstructions])
     }
     const grid = new gridjs.Grid({
       columns:[{
         name:'Name',
-        width: 150,
-        resizable: true,
+        width: '100px',
         sort: true,
       }, {
         name: 'Equipment', 
-        width: 200,
-        resizable: true,
+        width: '100px',
+        
       }, {
         name:'Muscle',
-        width: 150,
-        resizable: true,
+        width: '80px',
       }, {
-        name:'Instructions'
-        
+        name:'Instructions',
+        width: '250px',
+
       }],
+      resizable: true,
       search: true,
       fixedHeader: true,
       height: '500px',
       data: exercise,
 
     }).render(document.getElementById('grid'));
-
-    document.getElementById("grid").style.textTransform = "capitalize";
-
   }
   catch (error){
     console.log(error);
